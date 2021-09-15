bdd3e6636d4d730b068dc9769d714d4b09129693
morio77 <howtostudyy77@gmail.com>
2021-09-15 23:45:05 +0900

    AndroidでFirebase使えるように微修正

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:flutter_auth_ui/flutter_auth_ui.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
